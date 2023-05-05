ce673838c2fcff9045ffa25cbccc520647b6027c
Martin van Dam <martinvandam@protonmail.com>
2023-05-05 11:00:39 +0200

    force notifications to render client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Notifications from "@/components/notifications";
+import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const DynamicNotifications = dynamic(
+  () => import("@/components/notifications"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Notifications />
+      <DynamicNotifications />
     </main>
   );
 }
