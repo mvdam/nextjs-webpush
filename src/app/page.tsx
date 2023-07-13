b8671e50be10044e12cdaacad4c9b712cb8a6c5b
Martin van Dam <martinvandam@protonmail.com>
2023-07-13 12:45:08 +0200

    fix: force Notifications to be rendered client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import Notifications from '@/app/components/notifications'
+import dynamic from 'next/dynamic'
+
+const Notifications = dynamic(() => import('@/app/components/notifications'), {
+  ssr: false,
+})
 
 export default function Home() {
   return <Notifications />
